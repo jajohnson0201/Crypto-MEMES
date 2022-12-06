4291c40eb0f18c164f0b8b440bd367b916383162
joe <jajohnson0201@gmail.com>
2022-12-05 19:48:15 -0600

    added event listener for search

--- assets/script.js
+++ assets/script.js
@@ -158,83 +158,64 @@ function searchingCoin() {
         });
     return coinInfo
 }
-searchingCoin();
-
-function getCoinsInfo() {
-    var coinsInfo;
-    fetch('https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0', options)
-        .then(function (response) {
-            if (response.ok) {
-                response.json().then(function (data) {
-                    console.log(data.data.coins)
-                    console.log(data.data.coins.length)
-                    coinsInfo = data.data.coins
-                    console.log("inside data")
-                        function getCoinsNames () {
-                                for(var n = 0; n < coinsInfo.length; n++){
-                                    console.log(coinsInfo[n].name)
-                                }
-                        }
-                    getCoinsNames();
-                })
-            }
-        })
-        .catch(function (err) {
-            console.error(err)
-        });
 
 
 //GIF fetch  
-  //var GifAPIkey = 'S3VasUEiDf5XXAyFPT9xCzfa0unw9jt3' this key is already included in the GifURLrequest
-  var cryptoValue = 0 //positive, neutral, or negative value change of searched cryptocurrency
-  //var GifURLrequest = 'https://api.giphy.com/v1/gifs/search?api_key=S3VasUEiDf5XXAyFPT9xCzfa0unw9jt3&q=' + cryptoValue + '&limit=25&offset=0&rating=g&lang=en';
+//var GifAPIkey = 'S3VasUEiDf5XXAyFPT9xCzfa0unw9jt3' this key is already included in the GifURLrequest
+var cryptoValue = 0 //positive, neutral, or negative value change of searched cryptocurrency
+//var GifURLrequest = 'https://api.giphy.com/v1/gifs/search?api_key=S3VasUEiDf5XXAyFPT9xCzfa0unw9jt3&q=' + cryptoValue + '&limit=25&offset=0&rating=g&lang=en';
 
 
 if (cryptoValue === 1) {
-  //search for Gifs labeled with the tag 'celebrate'
-  var GifURLrequest = 'https://api.giphy.com/v1/gifs/search?api_key=S3VasUEiDf5XXAyFPT9xCzfa0unw9jt3&q=celebrate&limit=25&offset=0&rating=g&lang=en';
-  fetch(GifURLrequest)
+    //search for Gifs labeled with the tag 'celebrate'
+    var GifURLrequest = 'https://api.giphy.com/v1/gifs/search?api_key=S3VasUEiDf5XXAyFPT9xCzfa0unw9jt3&q=celebrate&limit=25&offset=0&rating=g&lang=en';
+    fetch(GifURLrequest)
     //return json results
     .then(function (response) {
-      return response.json();
+        return response.json();
     })
     .then(function(data) {
-      console.log(data.data[0].images.original.mp4); //<--- this is the path to the url for the mp4 that will be displayed
+        console.log(data.data[0].images.original.mp4); //<--- this is the path to the url for the mp4 that will be displayed
     })
-  } else if (cryptoValue === -1) {
-  //searhc for Gifs labeled with the tag 'oh no'
-  var GifURLrequest = 'https://api.giphy.com/v1/gifs/search?api_key=S3VasUEiDf5XXAyFPT9xCzfa0unw9jt3&q=oh+no&limit=25&offset=0&rating=g&lang=en';
-  fetch(GifURLrequest)
+} else if (cryptoValue === -1) {
+    //searhc for Gifs labeled with the tag 'oh no'
+    var GifURLrequest = 'https://api.giphy.com/v1/gifs/search?api_key=S3VasUEiDf5XXAyFPT9xCzfa0unw9jt3&q=oh+no&limit=25&offset=0&rating=g&lang=en';
+    fetch(GifURLrequest)
     //return json results
     .then(function (response) {
-      return response.json();
+        return response.json();
     })
     .then(function(data) {
-      console.log(data.data[2].images.original.mp4) //<--- this is the path to the url for the mp4 that will be displayed
+        console.log(data.data[2].images.original.mp4) //<--- this is the path to the url for the mp4 that will be displayed
     })
-  } else {
-  //searhc for Gifs labeled with the tag 'unsure'
+} else {
+    //searhc for Gifs labeled with the tag 'unsure'
     var GifURLrequest = 'https://api.giphy.com/v1/gifs/search?api_key=S3VasUEiDf5XXAyFPT9xCzfa0unw9jt3&q=unsure&limit=25&offset=0&rating=g&lang=en';
     fetch(GifURLrequest)
-      //return json results
-      .then(function (response) {
+    //return json results
+    .then(function (response) {
         return response.json();
-      })
-      .then(function(data) {
+    })
+    .then(function(data) {
         console.log(data.data[1].images.original.mp4) //<--- this is the path to the url for the mp4 that will be displayed
-      })
-    } ;
+    })
+} ;
 
-  //to randomize gifs: use a random number generator to choose number from array (of 25?), use the random array number in the path to the mp4 url
-
-    return coinsInfo
-}
+//to randomize gifs: use a random number generator to choose number from array (of 25?), use the random array number in the path to the mp4 url
 
 
-//getCoinsNames();
 
-var coinsNames = getCoinsInfo();
-console.log(coinsNames);
-//console.log(searchingCoin());
 
+
+
+var searchButton = document.querySelector(".button");
+var input = document.querySelector(".input");
+
+searchButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    var typedInput = input.value;
+    searchingCoin();
+
+    
+});
 
